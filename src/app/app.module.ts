65bd02e4b44a484215c89ce258d1e7741388a3bd
Joshua Alzate <joshua@hlbb.hongleong.com.my>
2020-04-22 17:42:38 +0800

    Add Layout Add Navigation Bar Add Home Introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { LayoutComponent } from './layout/layout.component';
+import { NavigationBarComponent } from './layout/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LayoutComponent,
+    NavigationBarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
